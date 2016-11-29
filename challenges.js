8401823bd44bf2c106886dc9e86bda5601a1ef6f
Danny Libin <omegasol11@gmail.com>
2016-11-29 17:12:28 -0600

    animal picker challenge

--- challenges.js
+++ challenges.js
@@ -1,5 +1,27 @@
 const challenges = [
   {
+    number: 0,
+    name: "#0 Welcome to 'Hour of Code'!",
+    instructions: [
+      "In this adventure, we will be playing with an animal using code!",
+      "Choose your animal, and its color:",
+      "Final set of instructions for this challenge"
+    ],
+    seedCode: [
+      "<canvas id=pane width=300 height=200></canvas>",
+      "<script type='text/javascript'>",
+      "var canvas = document.getElementById('pane');",
+      "var context = canvas.getContext('2d');",
+      "context.fillStyle = 'rgb(250,0,0)';",
+      "context.fillRect(10, 10, 55, 50);",
+      "context.fillStyle = 'rgba(0, 0, 250, 0.5)';",
+      "context.fillRect(30, 30, 55, 50);",
+      "</script>"
+    ],
+    // array of functions to test the challenge?
+    tests: []
+  },
+  {
     number: 1,
     name: "#1 Welcome to 'Hour of Code'!",
     instructions: [
