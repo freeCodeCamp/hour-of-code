9f257c6b2fb226881a9301fe196bee534c60741f
Danny Libin <omegasol11@gmail.com>
2016-11-29 11:53:10 -0600

    remove codemirror logic from main html to update

--- codemirror-update.js
+++ codemirror-update.js
@@ -5,9 +5,8 @@ $(() => {
 var delay = 0;
 // Initialize CodeMirror editor with a nice html5 canvas demo.
 var editor = CodeMirror.fromTextArea(document.getElementById('code'), {
-  mode: 'text/html',
+  mode: 'htmlmixed',
   lineNumbers: true,
-  htmlMode: true,
   theme: "mdn-like"
 });
 
@@ -24,6 +23,32 @@ editor.on("change", function() {
   delay = setTimeout(updatePreview, 300);
 });
 
-setTimeout(updatePreview, 300);
+initEditor();
+//setTimeout(updatePreview, 300);
+
+function initEditor() {
+  initialValue = `<!doctype html>
+<html>
+  <head>
+    <meta charset=utf-8>
+    <title>HTML5 canvas demo</title>
+    <style>p {font-family: monospace;}</style>
+  </head>
+  <body>
+    <p>Canvas pane goes here:</p>
+    <canvas id=pane width=300 height=200></canvas>
+    <script>
+      var canvas = document.getElementById('pane');
+      var context = canvas.getContext('2d');
+      context.fillStyle = 'rgb(250,0,0)';
+      context.fillRect(10, 10, 55, 50);
+      context.fillStyle = 'rgba(0, 0, 250, 0.5)';
+      context.fillRect(30, 30, 55, 50);
+    </script>
+  </body>
+</html>
+  `;
+  editor.setValue(initialValue);
+}
 
 });
